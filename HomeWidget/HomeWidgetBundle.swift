14506d72a3da730969799f3d20c3ae6b32766d83
ThangTQ <thangtq@fabbi.com.vn>
2022-12-09 15:10:54 +0700

    base widget

---
+++ HomeWidget/HomeWidgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  HomeWidgetBundle.swift
+//  HomeWidget
+//
+//  Created by ThangTQ on 09/12/2022.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct HomeWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        HomeWidget()
+    }
+}
